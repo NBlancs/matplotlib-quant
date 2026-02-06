d0f01d367488a42bd13bb3518ec60a1ab561c8f0
NoL <bnoel.omar@gmail.com>
2026-02-07 01:37:11 +0800

    basic plotting

---
+++ first.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+
+
+# Simple plot
+x = [5,10,25,30,35,40]
+y = [10,20,30,40,50,60]
+
+plt.plot(x,y, color = "blue", marker = "o")
+plt.title ("X & Y Axis")
+plt.xlabel ("X Axis")
+plt.ylabel ("Y Axis")
+plt.grid()
+plt.show()
+
+
+
+
+
